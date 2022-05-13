96841a6a960f0a28c1e6882531ee10ac3604fc25
McKB <mckennakb@comcast.net>
2022-05-12 22:10:53 -0400

    adds utils folder, separates util functions from controllers, adds 404 catch-all

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,5 @@
 const movies = require('../movies')
+const { findMovieByTitle, findMovieByDirector, removeDuplicates, validateInput } = require('../utils/movieFuntions')
 
 const getAllMovies = (req, res) => {
   return res.status(201).send(movies)
@@ -6,9 +7,7 @@ const getAllMovies = (req, res) => {
 
 const getMovieBySearch = (req, res) => {
   const { search } = req.params
-  // search through titles and directors
   let foundMovieTitle = findMovieByTitle(search)
-
   let foundMovieDirector = findMovieByDirector(search)
 
   if (foundMovieTitle.length || foundMovieDirector.length) {
@@ -17,72 +16,21 @@ const getMovieBySearch = (req, res) => {
     movieList = removeDuplicates(movieList)
 
     return res.status(201).send(movieList)
-  }
-
-  else {
+  } else {
     return res.status(404).send('Not on our list :/')
   }
 }
 
 const addMovie = (req, res) => {
-  const {
-    title, directors, releaseDate, rating, runTime, genres
-  } = req.body
+  const newMovie = req.body
 
-  if (!title || !directors || !releaseDate || !rating || !runTime || !genres) {
+  if (!validateInput(newMovie)) {
     return res.status(400).send('Required: title, directors, releaseDate, rating, runTime, genres')
   }
-
-  const newMovie = req.body
 
   movies.push(newMovie)
 
   return res.status(201).send(newMovie)
 }
 
-
-const findMovieByTitle = (search) => {
-  let foundMovies = movies.filter(movie => {
-    const { title } = movie
-
-    if (title.toLowerCase().includes(search)) {
-      return true
-    } else {
-      return false
-    }
-  })
-
-  return foundMovies
-}
-
-const findMovieByDirector = (search) => {
-  let foundMovies = movies.filter(movie => {
-    const { directors } = movie
-
-    for (let director of directors) {
-      if (director.toLowerCase().includes(search)) {
-        return true
-      }
-    }
-
-    return false
-  })
-
-  return foundMovies
-}
-
-const removeDuplicates = (movieList) => {
-  let newList = []
-
-  for (let movie of movieList) {
-    if (!newList.includes(movie)) {
-      newList.push(movie)
-    }
-  }
-
-  return newList
-}
-
-
 module.exports = { getAllMovies, getMovieBySearch, addMovie }
-
--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/movies/:search', getMovieBySearch)
 
 app.post('/movies', express.json(), addMovie)
 
+app.get('*', (req, res) => {
+  res.status(404).send('Sorry, not found. Try searching for a movie or a director by typing /movies/your-input-here.')
+})
+
 app.listen(1337, () => {
   console.log('listening at http://localhost:1337') // eslint-disable-line no-console
 })
